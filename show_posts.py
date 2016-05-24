9e452684b344b743457aa4b3f510ebced451f990
Yoni Mood <yonispark@gmail.com>
2016-05-24 12:25:59 +0300

    bug fix and show post docopt

--- show_posts.py
+++ show_posts.py
@@ -1,3 +1,20 @@
+'''show_posts 1.0
+
+Usage:
+  show_posts id <id>
+  show_posts id <id> limit <limit>
+  show_posts -h | --help
+  show_posts --version
+  show_posts --reset
+
+Options:
+
+    show_posts is a mongo db class that gets the posts for the name given
+  --version     version 1.0
+  id      the name of the posts creator (without spaces).
+
+'''
+from docopt import docopt
 import pymongo
 import secret
 from templates import get_html_templtate
@@ -5,8 +22,6 @@ from pprint import pprint
 
 client = pymongo.MongoClient()
 db = client.get_database(secret.DB)
-
-posts = {}
 
 get_html_templtate(
     template_parameter="post",
@@ -22,14 +37,30 @@ get_html_templtate(
 ''')
 
 
-def get_post(creator_id, limit=100):
-    for index, post in enumerate(db.get_collection(secret.POSTS).find({{'creator_id': creator_id}})):
+def get_posts(creator_id, limit=100):
+    posts = {}
+    limit = int(limit)
+    for index, post in enumerate(db.get_collection(secret.POSTS).find()):
         if index == limit:
             break
         raw = get_html_templtate(**post, template_parameter="post")
         posts[post['id']] = raw
+    return posts
 
 
 # with open("index.html", "w", encoding="utf-8") as f:
 #     f.write(str(pages))
-pprint(posts)
+
+
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='show_posts 1.0')
+    if arguments['--reset']:
+        client = pymongo.MongoClient()
+        db = client.get_database(secret.DB)
+        db.drop_collection(secret.POSTS)
+    if arguments['id']:
+        posts = get_posts(arguments['id'], limit=arguments['<limit>']) if arguments['limit'] else get_posts(
+            arguments['id'])
+        pprint(posts)
